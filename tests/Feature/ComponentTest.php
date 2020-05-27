e26be16f03c0cbc8b94ee1380cdea749fa1b222c
Mustaf Hyseni <tafhyseni@gmail.com>
2020-05-27 02:58:42 +0200

    tests

---
+++ tests/Feature/ComponentTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tafhyseni\PhpGodaddy\Tests;
+
+use Tafhyseni\PhpGodaddy\Domain;
+
+it('returns empty array', function () {
+    $array = [];
+
+    assertEmpty($array);
+});
---
+++ tests/Pest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mustaf Hyseni
+ * Date: 5/27/2020
+ * Time: 2:54 AM
+ */
+
+namespace Tafhyseni\PhpGodaddy\Tests;
+
+
+class Pest
+{
+
+}

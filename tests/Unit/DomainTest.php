109ccae1da20d948b743bc147dbd5dfcc3a448c5
Mustaf Hyseni <tafhyseni@gmail.com>
2020-06-08 23:49:31 +0200

    Travis update

---
+++ tests/Unit/DomainTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tafhyseni\PhpGodaddy\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Tafhyseni\PhpGodaddy\Exceptions\DomainException;
+
+class DomainTest extends TestCase
+{
+    /** @test */
+    public function it_throws_api_exception()
+    {
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('API Key has not been provided!');
+        $this->expectExceptionCode(401);
+
+        throw DomainException::noApiKeyProvided();
+    }
+
+    /** @test */
+    public function it_parses_domain()
+    {
+        $registrableDomain = \Tafhyseni\PhpGodaddy\Actions\MyDomain::parse('http://www.mybestdomain.com')->getRegistrableDomain();
+        assertEquals('mybestdomain.com', $registrableDomain);
+    }
+}

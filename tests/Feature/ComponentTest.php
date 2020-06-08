876d1ba65df4098f66d40d25fff3a12dce5ec31a
Mustaf Hyseni <tafhyseni@gmail.com>
2020-06-08 23:21:17 +0200

    Add travis settings

--- tests/Feature/ComponentTest.php
+++ tests/Feature/ComponentTest.php
@@ -5,8 +5,19 @@ namespace Tafhyseni\PhpGodaddy\Tests;
 use Tafhyseni\PhpGodaddy\Domain;
 use Tafhyseni\PhpGodaddy\Exceptions\DomainException;
 
-it('throws our custom exception', function () {
+it('throws custom exception', function() {
+   throw DomainException::invalidDomainPeriod();
+})->throws(DomainException::class);
 
-//        assertInstanceOf();
-    //assertInstanceOf(DomainException::noSecretKeyProvided(), Domain::initialize());
+test('no api key custom exception', function () {
+    $this->expectException(DomainException::class);
+    $this->expectExceptionMessage('API Key has not been provided!');
+
+    throw DomainException::noApiKeyProvided();
+});
+
+it('parses domain', function () {
+    $registrableDomain = \Tafhyseni\PhpGodaddy\Actions\MyDomain::parse('http://www.mybestdomain.com')->getRegistrableDomain();
+    assertEquals('mybestdomain.com', $registrableDomain);
+
 });
